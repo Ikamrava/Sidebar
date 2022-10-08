4e94671d09dabb9369ac1f90b7558b2c6f59bfb7
Ikamrava <73425274+Ikamrava@users.noreply.github.com>
2022-10-08 17:08:00 +0100

    overlay

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const hamburgerMenu = document.querySelector(".hamburger");
 const container = document.querySelector(".container");
 const body = document.querySelector("body");
+const  overlay = document.getElementById("overlay");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
   container.classList.toggle("active");
+  overlay.classList.toggle("hidden");
+
 });
